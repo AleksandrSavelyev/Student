84ae706afd63a895c80025517d56abdc9959a6c0
AleksandrSavelyev <punk558721973@gmail.com>
2020-09-10 12:24:16 +0300

    coment

---
+++ Student/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            Student first = new Student(n);
+            int w = 0;
+
+            for (; ; )
+            {
+                Console.WriteLine("нажмите 1 чтобы добавить Студента");
+                Console.WriteLine("нажмитье 2 для вывода информации об аспирантах");
+                Console.WriteLine("нажмите 3 для вывода информации о студенте");
+                Console.WriteLine("нажмите 4 для выхода");
+               
+                string myMenyu = Console.ReadLine();                
+                switch (myMenyu)
+                {
+                    case "1": first = AddStu(first, n, w);
+                        w++;
+                        break;
+                    case "2":
+                        break;
+                    case "3":PrintInfoStu(first);
+                        break;
+                    default:
+                        break;
+                }
+
+                
+            }
+        }
+        static bool CheckNum(string a)
+        {            
+                if (int.TryParse(a, out int b))
+                    return true;
+                else
+                    return false;            
+        }
+        static bool CheckStr(string a)
+        {            
+                char[] ch = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };                
+                for(int i=0;i<a.Length;i++)
+                {
+                    for(int j=0;j<ch.Length;j++)
+                    {
+                    if (a[i] == ch[j])
+                        return false;
+                    }
+                }
+            return true;
+        }
+        static int InNum()
+        {
+            for (; ; )
+            {
+                string a = Console.ReadLine();
+                bool b = CheckNum(a);
+                if (b == true)
+                {
+                   int c = Convert.ToInt32(a);
+                    return c;
+                }
+                else
+                    Console.Write("не коректные данные повторите ввод:\t");
+            }
+        }
+        static string InStr()
+        {
+            for(; ; )
+            {
+                string a = Console.ReadLine();
+                bool b = CheckStr(a);
+                if (b == true)
+                    return a;
+                else
+                    Console.Write("не коректные данные повторите ввод:\t");
+            }
+        }
+        static string InZachet()
+        {
+            for (; ; )
+            {
+                int a = InNum();
+                if (a < 10000000 && a > 99999999)
+                {
+                    string b = Convert.ToString(a);
+                    return b;
+                }
+                else
+                    Console.Write("номер зачетной книжки дожен быть не меньше и не длиннее 8символов");
+            }
+        }        
+        public static Student AddStu(Student first, int n, int i)
+        {
+            
+            Console.WriteLine(i);
+            for (; ; )
+            {
+                
+                first[i] = new Student();
+                Console.Write("введите имя студента:\t");
+                first[i].Name = InStr();
+                Console.Write("введите фамилию студента:\t");
+                first[i].LastName = InStr();
+                Console.Write("введите курс обучения:\t");
+                first[i].Kurs = InNum();
+                Console.Write("введите номер зачетной книжки:\t");
+                first[i].Zachet = InNum();
+               
+                Console.Write("чтобы добавить ещё 1 студента нажмите 1:\t");
+                Console.Write("чтобы выйти в меню нажмите любую цифру:\t");
+                int a = InNum();
+                if (a == 1)
+                {
+                    AddStu(first, n, i+1);
+                }
+                else
+                    break;
+            }
+            return first;
+            
+        }
+        static void AddAsp()
+        {
+            Aspirant firstAspirant = new Aspirant();
+            Console.Write("введите имя аспиранта:\t");
+            firstAspirant.Name = InStr();
+            Console.Write("введите фамилию аспирантиа:\t");
+            firstAspirant.LastName = InStr();
+            Console.Write("введите курс обучения:\t");
+            firstAspirant.Kurs = InNum();
+            Console.Write("введите номер зачетной книжки:\t");
+            firstAspirant.Zachet = InNum();
+            Console.Write("введите название дисертации:\t");
+            firstAspirant.Diss = InStr();
+            firstAspirant[Aspirant.indexAsp] = new Aspirant();
+        }
+        static void PrintInfoStu(Student first)
+        {
+            Console.Write("введите номер студента для вывода информации:\t");
+            int a = InNum();
+            first[a].PrintInfo();
+        }
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student
+{
+    class Student
+    {
+        private string name;
+        private string lastName ;
+        private int kurs;
+        private int zachet;
+        private int n;
+        //public static int index=0;
+        public Student[] data;
+        public Student() { }
+        public Student(int n)
+        {
+            data = new Student[n];
+        }
+        public Student(string name,string lastName,int kurs,int zachet,int n)
+        {
+            data = new Student[n];
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public int Kurs
+        {
+            get { return kurs; }
+            set { kurs = value; }
+        }
+        public int Zachet
+        {
+            get { return zachet; }
+            set { zachet = value; }
+        }
+        public Student this[int index ]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+       /* public static void InnIndex()
+        {
+            index += 1;
+        }*/
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"имя фамилия:{name} {lastName} курс: {kurs} номер зачетной книжки: {zachet}");
+        }
+    }
+    class Aspirant:Student
+    {
+        private string diss;
+        public static int indexAsp=0;
+        private Aspirant[] dataAsp;
+        public Aspirant() { }
+        public Aspirant(string name,string lastName,int kurs,int zachet,string diss,int n)
+            :base(name,lastName,kurs,zachet,n)
+        {
+
+        }
+        public string Diss
+        {
+            get { return diss; }
+            set { diss=value;}
+        }
+        public new Aspirant this[int indexAsp]
+        {
+            get
+            {
+                return dataAsp[indexAsp];
+            }
+            set
+            {
+                dataAsp[indexAsp] = value;
+            }
+        }
+        public new void PrintInfo()
+        {
+            Console.WriteLine($"имя фамилия студента: {Name} {LastName} номер зачетной книги: {Zachet} работает над дисертацией: {diss}");
+        }
+    }
+}
